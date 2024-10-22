3fda6af34864e3930ff12fec1c44e9c23c8f4084
Tatiana Kolodkina <ayriesudsu@gmailc.com>
2024-10-22 21:22:28 +0300

    final version

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,72 @@ function App() {
   const [todos, setTodos] = useState([])
   const [todoValue, setTodoValue] = useState('')
 
+  function persistData(data) {
+    try {
+      localStorage.setItem("todos", JSON.stringify(data));
+    } catch (error) {
+      console.error("Ошибка сохранения в локальное хранилище: ", error);
+    }
+  }
+  
+  function loadTodosFromLocalStorage() {
+    try {
+      const storedTodos = localStorage.getItem('todos');
+      if (storedTodos) {
+        const parsedTodos = JSON.parse(storedTodos);
+        if (Array.isArray(parsedTodos)) {
+          setTodos(parsedTodos);
+        } else {
+          console.warn('Некорректные данные в локальном хранилище. Использована пустая коллекция.');
+          setTodos([]);
+        }
+      } else {
+        console.warn('Локальное хранилище пустое. Использована пустая коллекция.');
+        setTodos([]);
+      }
+    } catch (error) {
+      console.error('Ошибка чтения из локального хранилища: ', error);
+    }
+  }
+  
+  useEffect(() => {
+    loadTodosFromLocalStorage();
+  }, []);
+
   function handleAddTodos(newTodo) {
-    const newTodoList = [...todos, newTodo]
-    setTodos(newTodoList)
+    const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
+    setTodos(newTodoList);
   }
 
   function handleDeleteTodo(index) {
     const newTodoList = todos.filter((todo, todoIndex) => {
-      return todoIndex !== index
-    })
-    setTodos(newTodoList)
+      return todoIndex !== index;
+    });
+    persistData(newTodoList);
+    setTodos(newTodoList);
   }
 
   function handleEditTodo(index) {
-    const valueToBeEdited = todos[index]
-    setTodoValue(valueToBeEdited)
-    handleDeleteTodo(index)
-    document.getElementById('main-input-id').focus()
+    const valueToBeEdited = todos[index];
+    setTodoValue(valueToBeEdited);
+    handleDeleteTodo(index);
+    document.getElementById("main-input-id").focus();
   }
 
   return (
-    <>
-      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
-      <TodoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos} />
-    </>
+    <div>
+    <TodoInput
+      todoValue={todoValue}
+      setTodoValue={setTodoValue}
+      handleAddTodos={handleAddTodos}
+    />
+    <TodoList
+      handleEditTodo={handleEditTodo}
+      handleDeleteTodo={handleDeleteTodo}
+      todos={todos}
+    />
+  </div>
   )
 }
 
